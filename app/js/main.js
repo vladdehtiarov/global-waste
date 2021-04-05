50b54708254f36884c1330b6f4097124c22dff9d
vladdehtiarov <vladdegtarov1998@gmail.com>
2021-04-06 01:42:47 +0300

    buils 06/04

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,41 @@
 
+// Reward list
+
+const rewardList = document.querySelector('.reward-list');
+const reward = document.querySelector('.reward');
+const sendId = document.querySelector('.send-id');
+const phoneId = document.getElementById('input-phone-id');
+const sendModal = document.querySelector('.send-modal');
+
+
+
+if (reward) {
+    reward.addEventListener('click', () => {
+        rewardList.classList.remove('hide');
+    });
+}
+
+
+if (sendModal) {
+    sendModal.addEventListener('click', () => {
+        document.location.href = "another-coordinate.html";
+    });
+    
+}
+
+
 // Modal window
 const phone = document.getElementById('input-phone');
 const coordinate = document.getElementById('input-coordinate');
 const owerlay = document.querySelector('.owerlay');
 const modal = document.querySelector('.modal');
+const modalPhone = document.querySelector('.modal-phone');
 const modalClose = document.querySelector('.modal--close');
-const sendModal = document.querySelector('.send-modal');
 const modalInner = document.querySelector('.modal--inner');
 const modalForm = document.querySelector('.form');
-const sendForm = document.querySelector('.send-form');
+const sendForm = document.querySelectorAll('.send-form');
 const coordinateBtn = document.getElementById('id-coordinates');
+const idBtn = document.getElementById('id-btn');
 const id = document.getElementById('id');
 
 const d = function() {
@@ -22,11 +48,12 @@ function removeData() {
     modalForm.classList.remove('hide');
     modalInner.classList.add('hide'); 
     phone.value = '';
+    phoneId.value = '';
     coordinate.value = '';
 }
 
-function showModal() {
-    modal.classList.remove('hide');
+function showModal(modalHtml) {
+    modalHtml.classList.remove('hide');
     owerlay.classList.remove('hide');
     window.removeEventListener('popstate', d);
 }
@@ -34,6 +61,8 @@ function showModal() {
 function hideModal() {
     owerlay.classList.add('hide');
     modal.classList.add('hide');
+    modalPhone.classList.add('hide');
+    phoneId.value = '';
     window.removeEventListener('popstate', d);
 }
 
@@ -41,39 +70,81 @@ function stateActions() {
     window.addEventListener('popstate' , d);
 }
 
-coordinateBtn.addEventListener('click', function(e) {
-    setTimeout(stateActions, 1);
-    showModal();
-});
+if (coordinateBtn) {
+    coordinateBtn.addEventListener('click', function(e) {
+        setTimeout(stateActions, 1);
+        showModal(modal);
+    });
+}
 
-owerlay.addEventListener('click', function() {
-    this.classList.add('hide');
-    modal.classList.add('hide');
-    removeData();
-});
+if (idBtn) {
+    idBtn.addEventListener('click', function(e) {
+        setTimeout(stateActions, 1);
+        showModal(modalPhone);
+    });
+}
 
-modalClose.addEventListener('click', () => {
-    hideModal();
-    removeData(); 
-});
+if (owerlay) {
+    owerlay.addEventListener('click', function() {
+        this.classList.add('hide');
+        modal.classList.add('hide');
+        modalPhone.classList.add('hide');
+        removeData();
+    });
+}
 
-sendModal.addEventListener('click', () => {
-    hideModal();
-    removeData();
-});
+if (modalClose) {
+    modalClose.addEventListener('click', () => {
+        hideModal();
+        removeData();
+    });
+}
 
-phone.addEventListener('input', () => {
-    phone.style.border = '1px solid #3EC072';
-});
+if (sendModal) {
+    sendModal.addEventListener('click', () => {
+        hideModal();
+        removeData();
+    });
+}
 
-sendForm.addEventListener('click', (e) => {
-    e.preventDefault();
-    if(phone.value) {
-        id.innerText = phone.value;
-        modalForm.classList.add('hide');
-        modalInner.classList.remove('hide');   
-    } else {
-        phone.style.border = '1px solid red';
-    }
-});
+if (phone) {
+    phone.addEventListener('input', () => {
+        phone.style.border = '1px solid #3EC072';
+    });
+}
+
+if (phoneId) {
+    phoneId.addEventListener('input', () => {
+        phone.style.border = '1px solid #3EC072';
+    });    
+}
+
+if (sendForm) {
+    sendForm.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            if(phone.value) {
+                id.innerText = phone.value;
+                modalForm.classList.add('hide');
+                modalInner.classList.remove('hide');   
+            } else {
+                phone.style.border = '1px solid red';
+            }
+    
+            if(phoneId.value) {
+                id.innerText = phoneId.value;
+                modalPhone.classList.add('hide');
+                modal.classList.remove('hide');
+                modalForm.classList.add('hide');
+                modalInner.classList.remove('hide');   
+            } else {
+                phone.style.border = '1px solid red';
+            }
+    
+        });
+    });
+}
+
+
+
 
